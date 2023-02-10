f637dcfcd41c0d91b7c21eb87641081d9011f422
Neto test <anastasiya.parfinenko@gmail.com>
2023-02-10 16:46:45 +0600

    all

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int amount = 1500;
+        int initial = 300;
+        int balance = (amount + initial);
+
+        int bonus;
+        if (amount >= 1000) {
+            bonus = (amount / 100);
+        } else {
+            bonus = 0;
+        }
+
+        int overallBalance = (bonus + balance);
+        System.out.println("Итоговый баланс:" + overallBalance);
+        System.out.println("Бонус:" + bonus);
     }
+
 }
+
